69fc30ed04bce03b83c2fa69b226c727104849b0
Niv BP <nivbp7@gmail.com>
2023-06-24 18:23:23 +0300

    Initial commit for Xcode project

---
+++ PassOniOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassOniOS
+//
+//  Created by niv ben-porath on 24/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
