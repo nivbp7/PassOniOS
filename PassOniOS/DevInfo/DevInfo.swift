d0fef46d23b3ee971d97d5212bbea3941ffb2228
Niv BP <nivbp7@gmail.com>
2023-07-04 13:27:31 +0300

    Add DevInfo
    This will be used to generate specific dev info, such as team ID.

---
+++ PassOniOS/DevInfo/DevInfo.swift
@@ -0,0 +1,13 @@
+//
+//  DevInfo.swift
+//  PassOniOS
+//
+//  Created by niv ben-porath on 04/07/2023.
+//
+
+import Foundation
+
+struct DevInfo {
+    static let passTypeIdentifier = ""
+    static let teamIdentifier = ""
+}
